3026a8d48ff20be40facf830c76c31f0c0cbb96d
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-08-14 16:56:51 +0800

    Create SnakeQueue.c

---
+++ SnakeQueue.c
@@ -0,0 +1,43 @@
+#include "SnakeQueue.h"
+#include <stdlib.h>
+
+struct SnakeBody* CreateSnakeBody(int row, int col);
+
+struct Snake* CreateSnake(int row, int col, int len){
+	struct Snake* snake = (struct Snake*)malloc(sizeof(struct Snake));
+	snake->head = CreateSnakeBody(row, col);
+	for(int i = 1; i < len ; ++i){
+		PushSnakeBody(row, col - i,snake);
+	}
+
+	return snake;
+}
+
+struct SnakeBody* CreateSnakeBody(int row, int col){
+	struct SnakeBody* body = (struct SnakeBody*)malloc(sizeof(struct SnakeBody));
+	body->next = NULL;
+	body->row = row;
+	body->col = col;
+	return body;
+}
+
+void PushSnakeBody(int row, int col, struct Snake* snake){
+	struct SnakeBody* tmp = snake->head;
+	struct SnakeBody* body =  CreateSnakeBody(row, col);
+
+	snake->head = body;
+	body->next = tmp;
+}
+
+void PopSnakeBody(struct Snake* snake, int *row, int *col){
+	struct SnakeBody* preNode = snake->head;
+	struct SnakeBody* curNode = snake->head;
+	while(curNode->next){
+		preNode = curNode;
+		curNode = curNode->next;
+	}
+	preNode->next = NULL;
+	*row = curNode->row;
+	*col = curNode->col;
+	free(curNode);
+}
