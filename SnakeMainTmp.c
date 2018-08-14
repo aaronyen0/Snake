66fe856ba47b8e8c890930aca729dbb145bbf9d2
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-08-14 17:20:48 +0800

    Update SnakeMainTmp.c

--- SnakeMainTmp.c
+++ SnakeMainTmp.c
@@ -1,3 +1,8 @@
+// test20180814.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include <varargs.h>
 #include <Windows.h>
 #include <conio.h>
 #include "QueueTool.h"
@@ -5,12 +10,14 @@
 
 enum eMapType{Space, Snake, Wall};
 enum eArrow{up, down, left, right, dead};
-static eArrow arrow = up;
+static eArrow gArrow = up;
 
 const int gHeight = 35;
 const int gWidth  = 60;
 
 static int gMap[gHeight][gWidth] = {(int)Space};
+
+DWORD WINAPI ReadKey(void* data);
 
 void gotoxy(int row, int column)
 {
@@ -43,7 +50,8 @@ void SetWall(int row, int column, char c){
 
 struct Snake* IniGmae(){
 	int x, y;
-	
+	int snakeSize = 8;
+
 	//設定windows console的buffer
 	COORD coord;
 	coord.X = gWidth;
@@ -60,8 +68,8 @@ struct Snake* IniGmae(){
 	x = gHeight / 2;
 	y = gWidth / 2;
 
-	struct Snake* s = CreateSnake(x, y, 8);
-	for(int i = 0; i < 4; ++i){
+	struct Snake* s = CreateSnake(x, y, snakeSize);
+	for(int i = 0; i < snakeSize; ++i){
 		printToCoordinates(x, y - i, '*');
 		gMap[x][y - i] = (int)Snake;
 	}
@@ -75,7 +83,7 @@ void Pop(struct Snake* snake){
 	gMap[x][y] = (int)Space;
 }
 
-void Push(eArrow arrow, struct Snake* snake){
+int Push(eArrow arrow, struct Snake* snake){
 	int x = snake->head->row;
 	int y = snake->head->col;
 	if(arrow == up){
@@ -87,12 +95,17 @@ void Push(eArrow arrow, struct Snake* snake){
 	}else if(arrow == right){
 		y++;
 	}else{
-		printf("dead");
-		return;
+		//printf("dead");
+		return -1;
 	}
+	if(gMap[x][y] != (int)Space){
+		return -1;
+	}
+
 	PushSnakeBody(x, y,  snake);
 	printToCoordinates(x, y, '*');
 	gMap[x][y] = (int)Snake;
+	return 0;
 }
 
 eArrow NextWay(struct Snake* snake){
@@ -118,20 +131,57 @@ eArrow NextWay(struct Snake* snake){
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int rv;
 	eArrow arrow;
+
 	struct Snake* snake = IniGmae();
 	srand(time(NULL));
 
+	CreateThread(NULL, 0, ReadKey, NULL, 0, NULL);
 	while(1){
-		Sleep(50);
+		Sleep(500);
 		Pop(snake);
-		arrow = NextWay(snake);
-		if(arrow == dead){
-			printf("dead");
+		//arrow = NextWay(snake);
+		
+		rv = Push(gArrow, snake);
+		if(rv < 0){
+			printf("you dead");
+			Sleep(1000);
 			break;
 		}
-		Push(arrow, snake);
 	}
 	return 0;
 }
 
+
+DWORD WINAPI ReadKey(void* data){
+	HANDLE handle;
+    DWORD cnt;//儲存出入
+    DWORD mode;//Console Mode
+    INPUT_RECORD input;
+
+    handle = GetStdHandle(STD_INPUT_HANDLE);//先取得基本輸入Handle
+    GetConsoleMode(handle, &mode);//得到基本輸入的Mode
+    SetConsoleMode(handle, mode & ~ENABLE_LINE_INPUT);//設定基本輸入模式，其中~ENABLE_LINE_INPUT代表不用按下Enter也可動作
+
+    while(ReadConsoleInput(handle, &input, 1, &cnt))//開始讀取使用者的動作
+    {
+        if(input.EventType == KEY_EVENT)
+        {
+            if(input.Event.KeyEvent.bKeyDown == TRUE)
+            {
+                if(input.Event.KeyEvent.wVirtualKeyCode == VK_UP)//大寫鎖定
+                    gArrow = up;
+                else if (input.Event.KeyEvent.wVirtualKeyCode == VK_DOWN)//Enter按鍵
+                    gArrow = down;
+                else if (input.Event.KeyEvent.wVirtualKeyCode == VK_LEFT)//Ctrl
+                    gArrow = left;
+                else if (input.Event.KeyEvent.wVirtualKeyCode == VK_RIGHT)//F4
+                    gArrow = right;
+            }
+        }
+		Sleep(1);
+    }
+
+    return 0;
+}
