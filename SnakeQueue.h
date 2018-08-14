ae36b2064bf8b01ec25bef6314b5831f920c0931
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-08-14 16:57:13 +0800

    Create SnakeQueue.h

---
+++ SnakeQueue.h
@@ -0,0 +1,15 @@
+#pragma once
+
+struct SnakeBody{
+	int row;
+	int col;
+	struct SnakeBody* next;
+};
+
+struct Snake{
+	struct SnakeBody* head;
+};
+
+struct Snake* CreateSnake(int row, int col, int len);
+void PushSnakeBody(int row, int col, struct Snake* snake);
+void PopSnakeBody(s
